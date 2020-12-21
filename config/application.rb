17b34d1fedd56ab7cf3ed55eb0fe12a76f8fdf0c
tjbigelow <tjbigelow@att.net>
2020-12-21 11:58:58 -0600

    moved cors back to initializer

--- config/application.rb
+++ config/application.rb
@@ -24,13 +24,6 @@ module PartygamesBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'https://tjb-partygame.herokuapp.com/',
-      'Access-Control-Allow-Methods' => 'POST, PUT, PATCH, DELETE, GET, OPTIONS',
-      'Access-Control-Request-Method' => '*',
-      'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    }
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
